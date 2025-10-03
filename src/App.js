46b06b99e5fbbb402d1a7d5228f1e68674ede433
Roberto Huerta <robertohuerta117@gmail.com>
2025-10-03 10:56:36 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [todos, setTodos] = useState ([
+    {id: 1, text: 'Learn React', completed: 'Completed' },
+    {id: 2, text: 'Build a Todo', completed: 'Completed' },
+    {id: 3, text: 'Submit Assignment', completed: 'Completed' }
+  ]);
   return (
     <h1>Task Management Project</h1>
   );
