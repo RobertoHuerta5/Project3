1899d3ce3f3c4fa5e606187b88afa8957b7a8b09
Roberto Huerta <robertohuerta117@gmail.com>
2025-10-03 22:36:21 -0500

    pdate

--- src/component/pages/ContactPage.jsx
+++ src/component/pages/ContactPage.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function ContactPage() {
+
+  const [firstName, setFirstName] = useState ("")
+  const [lastName, setLastName] = useState ("")
+  const [emailAdd, setEmailAdd] = useState ("")
+  const [commentText, setCommentText] = useState ("")
+
+
+  function handleSubmit () {
+    console.log(firstName, lastName, emailAdd, commentText)
+  }
+
   return (
     <>
-    <div>Contact Page</div>
-    <p>Tel: <br/>
-    email:
-    </p>
+      <h1>Contact Form</h1>
+        <label>First Name:
+          <input type="text" name="firstName" onChange={(event)=>setFirstName(event.target.value)}  ></input>
+        </label>
+
+        <label>Last Name:
+          <input type="text" name="lastName" onChange={(event)=>setLastName(event.target.value)}  ></input>
+        </label>
+
+        <label>Email Address:
+          <input type="text" name="emailAdd" onChange={(event)=>setEmailAdd(event.target.value)} ></input>
+        </label>
+
+        <label>Comments:
+          <input type="text" name="tel" onChange={(event)=>setCommentText(event.target.value)} ></input>
+        </label>
+
+      <button onClick={handleSubmit} >SUBMIT</button>
     </>
   )
 }
