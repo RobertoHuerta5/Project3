da08427e50d1ab2f2d20478d235a038f5e21d8f4
Roberto Huerta <robertohuerta117@gmail.com>
2025-10-07 15:32:30 -0500

    update

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -14,7 +14,6 @@ export default function Header() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            
             <Nav.Link href="/contact">Contact</Nav.Link>
             <Nav.Link href="/todo">To Do</Nav.Link>
           </Nav>
