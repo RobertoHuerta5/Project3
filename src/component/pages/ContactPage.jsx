c2c6da7ee1d22527c7ff94d7ac480e496ccb2654
Roberto Huerta <robertohuerta117@gmail.com>
2025-11-18 21:27:05 -0600

    update

--- src/component/pages/ContactPage.jsx
+++ src/component/pages/ContactPage.jsx
@@ -14,27 +14,28 @@ export default function ContactPage() {
 
   return (
     <>
-      <h1 className="mainTitle">Contact Form</h1>
-        <div className="contactForm">
-        <label>First Name:
-            <input type="text" name="firstName" onChange={(event)=>setFirstName(event.target.value)}  ></input>
-          </label>
+      <div className="contactPage"> 
+        <h1 className="mainTitle">Contact Form</h1>
+          <div className="contactForm">
+            <label>First Name:
+                <input type="text" name="firstName" onChange={(event)=>setFirstName(event.target.value)}  ></input>
+              </label>
 
-          <label>Last Name:
-            <input type="text" name="lastName" onChange={(event)=>setLastName(event.target.value)}  ></input>
-          </label>
+              <label>Last Name:
+                <input type="text" name="lastName" onChange={(event)=>setLastName(event.target.value)}  ></input>
+              </label>
 
-          <label>Email Address:
-            <input type="text" name="emailAdd" onChange={(event)=>setEmailAdd(event.target.value)} ></input>
-          </label>
+              <label>Email Address:
+                <input type="text" name="emailAdd" onChange={(event)=>setEmailAdd(event.target.value)} ></input>
+              </label>
 
-          <label>Comments:
-            <input type="text" name="tel" onChange={(event)=>setCommentText(event.target.value)} ></input>
-          </label>
+              <label>Comments:
+                <input type="text" name="tel" onChange={(event)=>setCommentText(event.target.value)} ></input>
+              </label>
 
-          <button onClick={handleSubmit} >SUBMIT</button>
-        </div>
-          
+            <button onClick={handleSubmit} >SUBMIT</button>
+          </div>
+      </div>
 
    
     </>
