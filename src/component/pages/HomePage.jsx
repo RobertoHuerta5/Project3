a243c3f9933e150f6cccd11dac30d6cf3fc9e7c5
Roberto Huerta <robertohuerta117@gmail.com>
2025-11-20 21:33:09 -0600

    update

--- src/component/pages/HomePage.jsx
+++ src/component/pages/HomePage.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function HomePage() {
   return (
     <>
-     <h1 className="mainTitle">Welcome to Task Management</h1>
+      <div className="homePage">
+        <h1 className="mainTitle">Welcome to Task Management</h1>
+      </div>
     </>
    
   )
